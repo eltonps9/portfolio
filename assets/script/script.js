9e65b3a23c1f11e2add5b6ac01dd2da7942deb82
Elton Passos <81651224+eltonps9@users.noreply.github.com>
2023-10-16 00:31:07 -0300

    Add files via upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,3 +23,20 @@ var link2 =document.querySelector(".buttonProj");
 link2.addEventListener('click',()=>{
     window.open();
 })
+
+
+
+
+
+
+function sobreMim(){
+    var button=document.querySelector('.maisSobre');
+    var body=document.querySelector('body');
+    if(button.style.display=="flex"){
+        button.style.display='none';
+        body.style.overflow='scroll';
+    }else{
+        button.style.display='flex';
+        body.style.overflow='hidden';
+    }
+}
